67e7cfb29e5d4dcf634b86afc7d43782820695b2
Javed <smile110@gmail.com>
2023-03-27 13:58:11 +0500

    Testing ci/cd

--- src/routes/common.js
+++ src/routes/common.js
@@ -1,30 +1,40 @@
 import { Navigate } from "react-router-dom";
 import { Login } from "../pages";
 
-export const REDIRECT_TO_LOGIN = () => { 
-    return [
-        {
-            path: "/*",
-            element:  <Navigate to="/login" />
-        },
-        {
-            path: "/login",
-            element:  <Login />,
-            exact: true
-        }
-    ]
-}
-
-export const REDIRECT_TO_DASHBOARD = (userType) => { 
-    return [
-        {
-            path: "/",
-            element: userType === 1 ? <Navigate to="/dashboard" /> : <Navigate to="/cpanel" />
-        },
-        {
-            path: "/login",
-            element: userType === 1 ? <Navigate to="/dashboard" /> : <Navigate to="/cpanel" /> ,
-            exact: true
-        }
-    ]
-}
+export const REDIRECT_TO_LOGIN = () => {
+  return [
+    {
+      path: "/*",
+      element: <Navigate to="/login" />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+      exact: true,
+    },
+  ];
+};
+// Testing
+export const REDIRECT_TO_DASHBOARD = (userType) => {
+  return [
+    {
+      path: "/",
+      element:
+        userType === 1 ? (
+          <Navigate to="/dashboard" />
+        ) : (
+          <Navigate to="/cpanel" />
+        ),
+    },
+    {
+      path: "/login",
+      element:
+        userType === 1 ? (
+          <Navigate to="/dashboard" />
+        ) : (
+          <Navigate to="/cpanel" />
+        ),
+      exact: true,
+    },
+  ];
+};
